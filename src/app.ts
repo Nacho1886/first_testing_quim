7786285d24b487147b8b50b4107ef34a4d7a8609
Nacho Martí <68851175+Nacho1886@users.noreply.github.com>
2022-11-21 17:33:38 +0100

    Easier than before

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,45 @@
+import { ICar } from "./interfaces/ICar";
+
 const fetch = require('node-fetch');
 
 
-interface ICars { // Interfaz para la clase
-    "cylinders": number,
-    "fuel_type": string,
-    "plate": string, //Validar los campos de matricula (formato: 1234ABC)
-    "make": string,
-    "model": string,
-    "year": number,
 
+
+const arrayModels: string[] = ["camry", "a8", "corolla", "a4"]
+
+interface Options {
+    headers: Headers
+}
+
+interface Headers {
+    'X-Api-Key': string
 }
 
 // https://api-ninjas.com/api/cars
-// https://api.api-ninjas.com/v1/cars?limit=2&model=???????? Falta model, consultar docu
+// https://api.api-ninjas.com/v1/cars?limit=1&model=???????? Falta model, consultar docu
 // Key DjQPZrkIBps8VzNuQuc+9A==cZm6vLMz0CON9CKb
-
 
 // npm install & npm test
 
+//"plate": string, //Validar los campos de matricula (formato: 1234ABC) e intriducirlos en la clase Car como nuevo parámetro en la segunda función, libre elección de crear otra función extra en este proceso
+class Car implements ICar {
+    
+}
 
+const arrayCars: Car[] = []
 
-
-
+// En la primera función debes realizar las peticiones a la API de coches, docu más arriba
+async function fetchData(url: string, options: Options, arrayModels:string[] ): Promise<ICar>  {
+  // Recibes por parámetros los parámetros habituales de una API y un array de modelos, devulves un objeto con tipado ICar
+    const soyElResultadoDeUnaPromesa: ICar = {"city_mpg": 18, "class": "midsize car", "combination_mpg": 21, "cylinders": 4, "displacement": 2.2, "drive": "fwd", "fuel_type": "gas", "highway_mpg": 26, "make": "toyota", "model": "camry", "transmission": "a", "year": 1993}
+    return soyElResultadoDeUnaPromesa // Borrar
+}
+//Creas instancias de la Clase Car, añadiendo objetos tipados ICar, añadiendo el parámetro mencionado arriba con su validación
+async function arrayCarsConstruct(stepsInArray: number): Promise<Car[]> {
+    fetchData()
+//Guardas en arrayCar los coches instanciados, sean los que sean
+    return arrayCars
+}
 
 
 module.exports = {fetchData, arrayCarsConstruct};
--- tests/app.test.js
+++ tests/app.test.js
@@ -8,16 +8,18 @@ const options = {
   }
 }
 
+const arrayModels = ["camry", "a8", "corolla", "a4"]
+
 
 test('Función que retorna una promesa', async () => {
   expect(typeof fetchData).toBe('function');
-  const data = await fetchData(ApiUrl, options);
+  const data = await fetchData(ApiUrl, options, arrayModels[0]);
   expect(data).toStrictEqual([{"city_mpg": 18, "class": "midsize car", "combination_mpg": 21, "cylinders": 4, "displacement": 2.2, "drive": "fwd", "fuel_type": "gas", "highway_mpg": 26, "make": "toyota", "model": "camry", "transmission": "a", "year": 1993}]);
 });// expect data Promise object
 
 test('Función que retorna un Array de la clase Car', async () => {
-  const data = await fetchData(ApiUrl, options);
+  const data = await fetchData(ApiUrl, options, arrayModels[0]);
   expect(typeof arrayCarsConstruct).toBe('function');
-  expect(arrayCarsConstruct(data) instanceof Array).toBe(true);
-  expect(arrayCarsConstruct(data)).not.toEqual(data);
+  expect(arrayCarsConstruct(4) instanceof Array).toBe(true);
+  expect(arrayCarsConstruct(4)).not.toEqual(data);
 })
